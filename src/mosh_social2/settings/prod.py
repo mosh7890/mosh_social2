b7e8246d18327b710e7720eadf2c0464ce350113
mosh7890 <mosh7890@gmail.com>
2019-04-23 19:38:38 +0300

    pythonanywhere db setup

--- src/mosh_social2/settings/prod.py
+++ src/mosh_social2/settings/prod.py
@@ -2,18 +2,36 @@ from .base import *
 import sentry_sdk as sentry
 from sentry_sdk.integrations.django import DjangoIntegration
 
+# DATABASES = {
+#     'default': {
+#         'ATOMIC_REQUESTS': True,
+#         'CONN_MAX_AGE': config('CONN_MAX_AGE', default=60),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('RDS_DB_NAME'),
+#         'USER': config('RDS_USERNAME'),
+#         'PASSWORD': config('RDS_PASSWORD'),
+#         'HOST': config('RDS_HOSTNAME'),
+#         'PORT': config('RDS_PORT'),
+#         'TEST': {
+#             'NAME': f'test_{config("RDS_DB_NAME")}',
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ATOMIC_REQUESTS': True,
         'CONN_MAX_AGE': config('CONN_MAX_AGE', default=60),
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('RDS_DB_NAME'),
-        'USER': config('RDS_USERNAME'),
-        'PASSWORD': config('RDS_PASSWORD'),
-        'HOST': config('RDS_HOSTNAME'),
-        'PORT': config('RDS_PORT'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': f'{config("PYTHONANYWHERE_USERNAME")}${config("PYTHONANYWHERE_DB_NAME")}',
+        'USER': config('PYTHONANYWHERE_USERNAME'),
+        'PASSWORD': config('PYTHONANYWHERE_PW'),
+        'HOST': config('PYTHONANYWHERE_HOSTNAME'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
         'TEST': {
-            'NAME': f'test_{config("RDS_DB_NAME")}',
+            'NAME': f'{config("PYTHONANYWHERE_USERNAME")}$test_{config("PYTHONANYWHERE_DB_NAME")}',
         },
     }
 }
